6e4674a2dba9cddbd33cffd10044f5b59a4bc395
raneemabutair <raneemtr94@gmail.com>
2021-04-01 19:01:03 +0300

    edit javascript to my HTML

---
+++ javascript.js
@@ -0,0 +1,13 @@
+
+         
+      alert ('Welcome to our website!')
+    
+      var username = prompt("Please share your name with us");
+        alert('Hello! '+ username + ' please enjoy your time with us');
+         
+     var coffeetype = prompt('If you are a Black coffee person type BC but if you are a Turkish coffee person type TC');
+     
+    
+
+     if (coffeetype=='BC') {alert ('You prefer to keep things simple. You are old school, patient, sometimes quiet, efficient and set in your ways <3');} 
+     else {alert("You are a traditionalist, maybe even an old soul. You have solid roots,Being in your company is a treat! <3");}      
